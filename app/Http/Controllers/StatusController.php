b4b0227ccf46859493ab789c5110de7143b684e6
hailedereje <hailedereje88@gmail.com>
2023-05-01 10:09:46 +0300

    statuse model

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -7,16 +7,6 @@ use Illuminate\Http\Request;
 
 class StatusController extends Controller
 {
-    private $fillable = ["name"];
-
-    static function getStatus(string $id){
-
-        $res = Status::findOrFail($id);
-        return response()->json(["status"=>$res],200);
-    }
-
-    static function setStatus(Status $status){
-        Status::create($status);
-    }
+    
 
 }
--- app/Models/Status.php
+++ app/Models/Status.php
@@ -8,7 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'name'
-    ];
+    protected $fillable = ["name"];
+
+    public static function getStatus(string $id){
+
+        $res = Status::findOrFail($id);
+        return response()->json(["status"=>$res],200);
+    }
+
+    public static function setStatus(Status $status){
+        Status::create($status);
+    }
 }
